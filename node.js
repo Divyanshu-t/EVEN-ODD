9312ca32a0fe8670bb20a2a9d6b2eeb469839203
Smily-zack <155127760+Smily-zack@users.noreply.github.com>
2024-10-31 04:02:45 -0400

    first commit

---
+++ node.js
@@ -0,0 +1,13 @@
+confirm("Check Number It is Even RO Odd");
+let user_input = parseInt(prompt("Enter Any Number "))
+
+if(user_input===0){
+    document.write(`You Enter = ${user_input}   `);
+ }else{
+        if(user_input>0){
+            document.write(`Your " ${user_input} " is Positive Number `);
+        }else if(user_input<0){
+            document.write(`Your " ${user_input} " is Negative Number `);
+        }
+    }
+   
